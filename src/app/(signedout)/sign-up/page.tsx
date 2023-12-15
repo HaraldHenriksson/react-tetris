4a38d2c639748238ac5c90affd01162baf06522b
Harald Henriksson <harald@espinagar.se>
2023-12-15 15:19:51 +0100

    sign-up page with similar design as the sign-in page

--- src/app/(signedout)/sign-up/page.tsx
+++ src/app/(signedout)/sign-up/page.tsx
@@ -1,7 +1,59 @@
-import React from "react";
+"use server";
 
-function Register() {
-  return <div>Register</div>;
+import Messages from "@/components/Messages";
+
+export default async function Signup() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-customBlue py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <h1 className="mt-6 text-center text-3xl font-extrabold text-white">
+            Create your account
+          </h1>
+        </div>
+        <form className="mt-8 space-y-6" action="/auth/sign-up" method="post">
+          <div className="rounded-md shadow-sm space-y-4">
+            <div>
+              <label htmlFor="email" className="sr-only">
+                Email address
+              </label>
+              <input
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                placeholder="Email address"
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                placeholder="Password"
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              >
+                Sign Up
+              </button>
+            </div>
+          </div>
+          <Messages />
+        </form>
+      </div>
+    </div>
+  );
 }
-
-export default Register;
