cfdbcb14f37d75aa364a54c9b89e885c1211ff2d
Harald Henriksson <harald@espinagar.se>
2023-12-15 15:28:25 +0100

    auth/forgot-password based on next.js doc

---
+++ src/app/auth/forgot-password/route.ts
@@ -0,0 +1,32 @@
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+export async function POST(request: Request) {
+  const requestUrl = new URL(request.url);
+  const formData = await request.formData();
+  const email = String(formData.get("email"));
+  const supabase = createRouteHandlerClient({ cookies });
+
+  const { error } = await supabase.auth.resetPasswordForEmail(email);
+
+  if (error) {
+    return NextResponse.redirect(
+      `${requestUrl.origin}/forgot-password?error=Could not send reset link`,
+      {
+        // a 301 status is required to redirect from a POST to a GET route
+        status: 301,
+      }
+    );
+  }
+
+  return NextResponse.redirect(
+    `${requestUrl.origin}/forgot-password?success=Reset link sent`,
+    {
+      // a 301 status is required to redirect from a POST to a GET route
+      status: 301,
+    }
+  );
+}
