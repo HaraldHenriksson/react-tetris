493227a3bfea149a81b9e1ce63377515be770317
Harald Henriksson <harald@espinagar.se>
2023-12-19 18:11:52 +0100

    initial game grid improvments

--- src/components/Gamegrid.tsx
+++ src/components/Gamegrid.tsx
@@ -1,29 +1,35 @@
-import React from "react";
+"use client";
 
-export default function Gamegrid() {
+import React, { useState, useEffect } from "react";
+
+const createGrid = (width: number, height: number) => {
+  return Array.from({ length: height * width }, () => 0);
+};
+
+const Cell = ({ value }: any) => {
+  const style = {
+    width: "20px",
+    height: "20px",
+    backgroundColor: value === 0 ? "white" : "blue",
+    border: "1px solid black",
+  };
+  return <div style={style} />;
+};
+
+const GameGrid = () => {
   const width = 10;
   const height = 20;
-  const grid = [];
-
-  for (let y = 0; y < height; y++) {
-    const row = [];
-    for (let x = 0; x < width; x++) {
-      row.push(0);
-    }
-    grid.push(row);
-  }
+  const [grid, setGrid] = useState(createGrid(width, height));
 
   return (
-    <div>
-      {grid.map((row, i) => (
-        <div key={i}>
-          {row.map((cell, j) => (
-            <span key={j} style={{ padding: "5px", border: "1px solid black" }}>
-              {cell}
-            </span>
-          ))}
-        </div>
+    <div
+      style={{ display: "grid", gridTemplateColumns: `repeat(${width}, 20px)` }}
+    >
+      {grid.map((value, index) => (
+        <Cell key={index} value={value} />
       ))}
     </div>
   );
-}
+};
+
+export default GameGrid;
