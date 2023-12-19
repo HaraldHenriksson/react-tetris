945e57b47af0fe20ba16e824e88afccdbd406448
Harald Henriksson <harald@espinagar.se>
2023-12-19 17:23:59 +0100

    navbar with stylin

--- src/app/(signedin)/page.tsx
+++ src/app/(signedin)/page.tsx
@@ -1,8 +1,12 @@
+"use server";
+
+import Navbar from "@/components/Navbar";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen bg-customBlue p-6">
+      <Navbar />
       <header className="text-center">
         <h1 className="text-4xl font-bold">React Tetris</h1>
         <p>A modern take on the classic game</p>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { getServerUser } from "@/app/lib/user/server";
+import { User } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+
+const NavItem = ({
+  href,
+  children,
+  className,
+}: {
+  href: string;
+  children: React.ReactNode;
+  className: string;
+}) => (
+  <li>
+    <a href={href} className={className}>
+      {children}
+    </a>
+  </li>
+);
+
+export default function Navbar() {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const userData = await getServerUser();
+      setUser(userData);
+    };
+
+    fetchUser();
+  }, []);
+
+  return (
+    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div className="hidden w-full md:block md:w-auto">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <NavItem
+              href="#"
+              className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+            >
+              Home
+            </NavItem>
+            <NavItem
+              href="#"
+              className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            >
+              Leaderboard
+            </NavItem>
+            <NavItem
+              href="#"
+              className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            >
+              {user?.email}
+            </NavItem>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
