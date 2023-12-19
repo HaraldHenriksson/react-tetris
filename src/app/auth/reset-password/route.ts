fb0cd1c828f38b90de1aa448a5a7533fab750064
Harald Henriksson <harald@espinagar.se>
2023-12-19 15:31:15 +0100

    route for updating new password

---
+++ src/app/auth/reset-password/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const requestUrl = new URL(request.url);
+  const formData = await request.formData();
+  const password = formData.get("password")?.toString();
+  const token = formData.get("token")?.toString();
+
+  const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY;
+
+  console.log({ password, token, supabaseServiceKey });
+  if (!token || !password || !supabaseServiceKey) {
+    return NextResponse.redirect(
+      `${requestUrl.origin}/reset-password?error=Invalid request`,
+      { status: 301 }
+    );
+  }
+
+  const response = await fetch(
+    "https://hpvvfhkykemmelhjpyqd.supabase.co/auth/v1/change-password",
+    {
+      method: "POST",
+      headers: new Headers({
+        "Content-Type": "application/json",
+        apikey: supabaseServiceKey,
+      }),
+      body: JSON.stringify({ token, password }),
+    }
+  );
+
+  if (!response.ok) {
+    return NextResponse.redirect(
+      `${requestUrl.origin}/reset-password?error=Failed to reset password`,
+      { status: 301 }
+    );
+  }
+
+  return NextResponse.redirect(`${requestUrl.origin}/login`, { status: 301 });
+}
