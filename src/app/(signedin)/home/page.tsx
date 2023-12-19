5422c44eaaa467c31841535bfbe58885f415b514
Harald Henriksson <harald@espinagar.se>
2023-12-19 17:54:09 +0100

    start game button

--- src/app/(signedin)/home/page.tsx
+++ src/app/(signedin)/home/page.tsx
@@ -12,6 +12,12 @@ export default async function Home() {
         <p>A modern take on the classic game</p>
       </header>
 
+      <Link legacyBehavior href="/game">
+        <a className="inline-block mt-4 px-6 py-2 text-white bg-blue-500 rounded hover:bg-blue-600">
+          Start Game
+        </a>
+      </Link>
+
       <footer className="text-gray-600">
         <p>React Tetris © 2023</p>
       </footer>
