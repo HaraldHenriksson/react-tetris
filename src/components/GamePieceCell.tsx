95a83780c2e9d34e0e1690b57c9293b132469e8f
Harald Henriksson <harald@espinagar.se>
2023-12-21 16:45:15 +0100

    build?

--- src/components/GamePieceCell.tsx
+++ src/components/GamePieceCell.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-// test
-
 export default function Cell({ filled }: { filled: boolean }) {
   const cellStyle = `w-8 h-8 ${
     filled ? "bg-blue-500 bg-opacity-80" : "bg-transparent"
