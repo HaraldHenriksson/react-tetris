bebe69b0a606e8397a7157efafab62f4b3d63d73
Harald Henriksson <harald@espinagar.se>
2023-12-21 14:31:23 +0100

    refactor gamePieces

--- src/components/GamePieces.tsx
+++ src/components/GamePieces.tsx
@@ -2,69 +2,29 @@
 
 import { useEffect, useState } from "react";
 import Cell from "./Cell";
+import Tetrominos from "./Tetromino";
 
-type Tetromino = keyof typeof Tetrominos;
+type TetrominoKey = keyof typeof Tetrominos;
 
-export default function GamePieces() {
-  const [tetromino, setTetromino] = useState<Tetromino>("I");
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-  const [rotation, setRotation] = useState(0);
+interface GamePiecesProps {
+  tetromino: TetrominoKey;
+  position: { x: number; y: number };
+  rotation: number;
+}
 
-  const moveLeft = () => {
-    // check collision
-    setPosition((prev) => ({ ...prev, x: prev.x - 1 }));
-  };
-
-  const moveRight = () => {
-    // check collision
-    setPosition((prev) => ({ ...prev, x: prev.x + 1 }));
-  };
-
-  const moveDown = () => {
-    // check collision
-    setPosition((prev) => ({ ...prev, y: prev.y + 1 }));
-  };
-
-  const rotate = () => {
-    // check collision
-    setRotation((prev) => (prev + 1) % 4);
-  };
-
-  useEffect(() => {
-    const handleKeyDown = (event: KeyboardEvent) => {
-      switch (event.key) {
-        case "ArrowLeft":
-          moveLeft();
-          break;
-        case "ArrowRight":
-          moveRight();
-          break;
-        case "ArrowDown":
-          moveDown();
-          break;
-        case "ArrowUp":
-          rotate();
-          break;
-        default:
-          break;
-      }
-    };
-
-    window.addEventListener("keydown", handleKeyDown);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  }, []);
-
+export default function GamePieces({
+  tetromino,
+  position,
+  rotation,
+}: GamePiecesProps) {
   const currentTetromino = Tetrominos[tetromino];
 
   return (
     <div
       style={{
         position: "absolute",
-        top: position.y * 20,
-        left: position.x * 20,
+        top: (position?.y || 0) * 20,
+        left: (position?.x || 0) * 20,
       }}
     >
       {currentTetromino.shape.map((row, rowIndex) => (
