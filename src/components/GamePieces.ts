b281af207e2209a8c3cfd5bc0856b18a9426ffd8
Harald Henriksson <harald@espinagar.se>
2023-12-21 12:39:42 +0100

    functions for game pieces

--- src/components/GamePieces.ts
+++ src/components/GamePieces.ts
@@ -1,4 +1,18 @@
+"use client";
+
+import { useState } from "react";
+
 export default function GamePieces() {
   const tetromino = "I";
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const moveLeft = () => {};
+
+  const moveRight = () => {};
+
+  const moveDown = () => {};
+
+  const rotate = () => {};
+
   return Tetrominos[tetromino];
 }
