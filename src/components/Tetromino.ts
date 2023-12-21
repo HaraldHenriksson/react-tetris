156446b4a730796a07402ff0166172ec5fe24df6
Harald Henriksson <harald@espinagar.se>
2023-12-21 12:24:40 +0100

    tetromino I shape

---
+++ src/components/Tetromino.ts
@@ -0,0 +1,12 @@
+const Tetrominos = {
+  0: { shape: [[0]], color: "0, 0, 0" },
+  I: {
+    shape: [
+      [0, "I", 0, 0],
+      [0, "I", 0, 0],
+      [0, "I", 0, 0],
+      [0, "I", 0, 0],
+    ],
+    color: "customBlue",
+  },
+};
