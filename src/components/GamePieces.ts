81abbda4e05dece9f61c8b66fa86a1099d8c0ba7
Harald Henriksson <harald@espinagar.se>
2023-12-21 12:41:08 +0100

    Implement moveLeft, moveRight, and moveDown functions in GamePieces.ts

--- src/components/GamePieces.ts
+++ src/components/GamePieces.ts
@@ -6,13 +6,17 @@ export default function GamePieces() {
   const tetromino = "I";
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
-  const moveLeft = () => {};
+  const moveLeft = () => {
+    setPosition((prev) => ({ ...prev, x: prev.x - 1 }));
+  };
 
-  const moveRight = () => {};
+  const moveRight = () => {
+    setPosition((prev) => ({ ...prev, x: prev.x + 1 }));
+  };
 
-  const moveDown = () => {};
-
-  const rotate = () => {};
+  const moveDown = () => {
+    setPosition((prev) => ({ ...prev, y: prev.y + 1 }));
+  };
 
   return Tetrominos[tetromino];
 }
