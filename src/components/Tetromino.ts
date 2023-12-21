8e8ba28869eec7c879df78094342914287970004
Harald Henriksson <harald@espinagar.se>
2023-12-21 12:58:03 +0100

    tetromino shapes

--- src/components/Tetromino.ts
+++ src/components/Tetromino.ts
@@ -9,4 +9,52 @@ const Tetrominos = {
     ],
     color: "customBlue",
   },
+
+  J: {
+    shape: [
+      [0, "J", 0],
+      [0, "J", 0],
+      ["J", "J", 0],
+    ],
+    color: "customOrange",
+  },
+  L: {
+    shape: [
+      [0, "L", 0],
+      [0, "L", 0],
+      [0, "L", "L"],
+    ],
+    color: "customPurple",
+  },
+  O: {
+    shape: [
+      ["O", "O"],
+      ["O", "O"],
+    ],
+    color: "customYellow",
+  },
+  S: {
+    shape: [
+      [0, "S", "S"],
+      ["S", "S", 0],
+      [0, 0, 0],
+    ],
+    color: "customGreen",
+  },
+  T: {
+    shape: [
+      [0, 0, 0],
+      ["T", "T", "T"],
+      [0, "T", 0],
+    ],
+    color: "customRed",
+  },
+  Z: {
+    shape: [
+      ["Z", "Z", 0],
+      [0, "Z", "Z"],
+      [0, 0, 0],
+    ],
+    color: "customWhite",
+  },
 };
