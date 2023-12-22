16f9434f47ed459c75f1575021ade48bf82b70ab
Harald Henriksson <harald@espinagar.se>
2023-12-22 12:09:04 +0100

    gamecollision component with props

--- src/components/GameCollision.tsx
+++ src/components/GameCollision.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+interface CheckCollisionProps {
+  newPosition: { x: number; y: number };
+  tetrominoShape: number[][];
+  grid: boolean[][];
+  gridWidth: number;
+  gridHeight: number;
+}
 
-export default function GameCollision() {
-  const checkCollision = (grid: any, piece: any) => {};
+export default function GameCollision({
+  newPosition,
+  tetrominoShape,
+  grid,
+  gridWidth,
+  gridHeight,
+}: CheckCollisionProps) {
+  const checkCollision = () => {
+    for (let y = 0; y < tetrominoShape.length; y++) {}
+    return false;
+  };
 
-  return <div>GameCollision</div>;
+  return null;
 }
