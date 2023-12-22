5280aaf04c5b17ef20e1e349d9cd76aed57f7439
Harald Henriksson <harald@espinagar.se>
2023-12-22 12:04:39 +0100

    checkCollition

--- src/app/(signedin)/game/page.tsx
+++ src/app/(signedin)/game/page.tsx
@@ -21,6 +21,14 @@ export default function Game() {
     createInitialGrid(gridWidth, gridHeight)
   );
 
+  const checkCollision = (
+    newPosition: { x: number; y: number },
+    tetrominoShape: number[][]
+  ) => {
+    for (let y = 0; y < tetrominoShape.length; y++) {}
+    return false;
+  };
+
   const settleTetromino = (
     tetrominoShape: number[][],
     position: { x: number; y: number }
