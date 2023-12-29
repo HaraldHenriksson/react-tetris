0d2429c994a9af28d26e4e05629e7a1abefbea03
Harald Henriksson <harald@espinagar.se>
2023-12-29 10:14:49 +0100

    score calculation functions

--- src/utils/scoreUtils.ts
+++ src/utils/scoreUtils.ts
@@ -1,1 +1,17 @@
-const SCORES = { 1: 40, 2: 100, 3: 300, 4: 1200 };
+const SCORES: { [key: number]: number } = { 1: 40, 2: 100, 3: 300, 4: 1200 };
+
+// Function to calculate score for line clears
+export const calculateScoreForLines = (
+  level: number,
+  linesCleared: number
+): number => {
+  const baseScore = SCORES[linesCleared] || 0;
+  return baseScore * level;
+};
+
+export const calculateTotalScore = (
+  currentScore: number,
+  additionalScore: number
+): number => {
+  return currentScore + additionalScore;
+};
