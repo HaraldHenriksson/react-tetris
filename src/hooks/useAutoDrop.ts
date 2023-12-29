8b27ca0f2794f0ed43b08bf12d4b9d84edf89281
Harald Henriksson <harald@espinagar.se>
2023-12-29 16:20:45 +0100

    useAutoDrop remake. using requestAnimationFreame and fix user input bug

--- src/hooks/useAutoDrop.ts
+++ src/hooks/useAutoDrop.ts
@@ -1,12 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 function useAutoDrop(moveDown: () => void, score: number) {
-  useEffect(() => {
-    const speed = 1000 - Math.min(Math.floor(score / 100) * 100, 500);
-    const drop = setInterval(moveDown, speed);
+  const moveDownRef = useRef(moveDown);
+  const lastTimeRef = useRef(performance.now());
+  const speedRef = useRef(1000 - Math.min(Math.floor(score / 100) * 100, 500));
 
-    return () => clearInterval(drop);
+  // Update the refs when moveDown/score changes
+  useEffect(() => {
+    moveDownRef.current = moveDown;
+    speedRef.current = 1000 - Math.min(Math.floor(score / 100) * 100, 500);
   }, [moveDown, score]);
+
+  useEffect(() => {
+    let animationFrameId: number;
+
+    const animate = (time: number) => {
+      if (time - lastTimeRef.current > speedRef.current) {
+        moveDownRef.current();
+        lastTimeRef.current = time;
+      }
+
+      animationFrameId = requestAnimationFrame(animate);
+    };
+
+    requestAnimationFrame(animate);
+
+    return () => {
+      cancelAnimationFrame(animationFrameId);
+    };
+  }, []);
 }
 
 export default useAutoDrop;
