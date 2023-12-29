1a10fa2a2e152f59c1c33cc8022767448de8641b
Harald Henriksson <harald@espinagar.se>
2023-12-29 11:35:57 +0100

    autodrop based on score

--- src/hooks/useAutoDrop.ts
+++ src/hooks/useAutoDrop.ts
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 function useAutoDrop(moveDown: () => void, score: number) {
   useEffect(() => {
-    const speed = 1000 - Math.min(score, 500);
+    const speed = 1000 - Math.min(Math.floor(score / 100) * 100, 500);
     const drop = setInterval(moveDown, speed);
 
     return () => clearInterval(drop);
