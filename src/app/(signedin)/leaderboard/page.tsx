64dc025747a866e08329a75a0df6c7a90efebb85
Harald Henriksson <harald@espinagar.se>
2024-01-08 16:07:11 +0100

    build fix?

--- src/app/(signedin)/leaderboard/page.tsx
+++ src/app/(signedin)/leaderboard/page.tsx
@@ -22,30 +22,14 @@ interface GameData {
 }
 
 export default async function Leaderboard() {
-  let topBestGames: GameData[];
-  let recentGames: GameData[];
-  let errorMessage: string = "";
-
-  try {
-    topBestGames = await fetchTopBestGames();
-    recentGames = await fetchRecentGames();
-  } catch (error) {
-    console.error("Error loading leaderboard data:", error);
-    topBestGames = [];
-    recentGames = [];
-    errorMessage = "Error fetching game data. Please try again later.";
-  }
+  const topBestGames = await fetchTopBestGames();
+  const recentGames = await fetchRecentGames();
 
   return (
     <div className="bg-customBlue min-h-screen p-8 text-white">
       <h1 className="text-4xl font-bold text-center mb-10 text-amber-600">
         Leaderboard
       </h1>
-
-      {errorMessage && (
-        <p className="text-red-500 text-center">{errorMessage}</p>
-      )}
-
       <div className="max-w-4xl mx-auto">
         <section>
           <h2 className="text-3xl mb-4 text-blue-500">Top Best Games</h2>
