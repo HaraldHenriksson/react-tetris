53fe8293ace0e259b58a30f86859237f0c5109d3
Harald Henriksson <harald@espinagar.se>
2024-01-08 12:13:54 +0100

    refactor leaderboard page component

--- src/app/(signedin)/leaderboard/page.tsx
+++ src/app/(signedin)/leaderboard/page.tsx
@@ -6,12 +6,19 @@ import {
   fetchTopBestGames,
 } from "../_server-actions/actions";
 
+interface UserData {
+  id: string;
+  email: string;
+}
+
 interface GameData {
   id: number;
   score: number;
   level: number;
   linesCleared: number;
   createdAt: Date;
+  userId: string;
+  user: UserData;
 }
 
 export default function Leaderboard() {
@@ -22,9 +29,11 @@ export default function Leaderboard() {
     const loadLeaderboardData = async () => {
       try {
         const topGames = await fetchTopBestGames();
+        console.log("topGames", topGames);
         setTopBestGames(topGames);
 
         const recent = await fetchRecentGames();
+        console.log("recent", recent);
         setRecentGames(recent);
       } catch (error) {
         console.error("Error loading leaderboard data:", error);
@@ -36,27 +45,67 @@ export default function Leaderboard() {
 
   return (
     <div className="bg-customBlue min-h-screen p-8 text-white">
-      <h1 className="text-4xl font-bold text-center mb-10">Leaderboard</h1>
+      <h1 className="text-4xl font-bold text-center mb-10 text-amber-600">
+        Leaderboard
+      </h1>
       <div className="max-w-4xl mx-auto">
         <section>
-          <h2 className="text-3xl mb-4">Top Best Games</h2>
+          <h2 className="text-3xl mb-4 text-blue-500">Top Best Games</h2>
           {topBestGames.map((game, index) => (
             <div
               key={index}
               className="bg-gray-800 bg-opacity-75 rounded-lg shadow-xl p-4 mb-4"
             >
-              {/* game data */}
+              <p>
+                User Email:{" "}
+                <span className="text-yellow-400">{game.user.email}</span>
+              </p>
+              <p>
+                Score: <span className="text-green-400">{game.score}</span>
+              </p>
+              <p>
+                Level: <span className="text-blue-400">{game.level}</span>
+              </p>
+              <p>
+                Lines Cleared:{" "}
+                <span className="text-red-400">{game.linesCleared}</span>
+              </p>
+              <p>
+                Date:{" "}
+                <span className="text-gray-400">
+                  {new Date(game.createdAt).toLocaleString()}
+                </span>
+              </p>
             </div>
           ))}
         </section>
         <section>
-          <h2 className="text-3xl mb-4">Recent Games</h2>
+          <h2 className="text-3xl mb-4 text-blue-500">Recent Games</h2>
           {recentGames.map((game, index) => (
             <div
               key={index}
               className="bg-gray-800 bg-opacity-75 rounded-lg shadow-xl p-4 mb-4"
             >
-              {/*  game data */}
+              <p>
+                User Email:{" "}
+                <span className="text-yellow-400">{game.user.email}</span>
+              </p>
+              <p>
+                Score: <span className="text-green-400">{game.score}</span>
+              </p>
+              <p>
+                Level: <span className="text-blue-400">{game.level}</span>
+              </p>
+              <p>
+                Lines Cleared:{" "}
+                <span className="text-red-400">{game.linesCleared}</span>
+              </p>
+              <p>
+                Date:{" "}
+                <span className="text-gray-400">
+                  {new Date(game.createdAt).toLocaleString()}
+                </span>
+              </p>
             </div>
           ))}
         </section>
