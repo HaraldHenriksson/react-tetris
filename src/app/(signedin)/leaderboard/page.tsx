668ca695c72c6119685da9444a21acf5137765a3
Harald Henriksson <harald@espinagar.se>
2024-01-08 15:45:22 +0100

    handle error loading data

--- src/app/(signedin)/leaderboard/page.tsx
+++ src/app/(signedin)/leaderboard/page.tsx
@@ -1,14 +1,22 @@
 "use server";
 
+import Messages from "@/components/Messages";
 import {
   fetchRecentGames,
   fetchTopBestGames,
 } from "../_server-actions/actions";
 
 export default async function Leaderboard() {
-  const topBestGames = await fetchTopBestGames();
+  let topBestGames: any[], recentGames: any[];
 
-  const recentGames = await fetchRecentGames();
+  try {
+    topBestGames = await fetchTopBestGames();
+    recentGames = await fetchRecentGames();
+  } catch (error) {
+    console.error("Error loading leaderboard data:", error);
+    topBestGames = [];
+    recentGames = [];
+  }
 
   return (
     <div className="bg-customBlue min-h-screen p-8 text-white">
@@ -18,63 +26,72 @@ export default async function Leaderboard() {
       <div className="max-w-4xl mx-auto">
         <section>
           <h2 className="text-3xl mb-4 text-blue-500">Top Best Games</h2>
-          {topBestGames.map((game, index) => (
-            <div
-              key={index}
-              className="bg-gray-800 bg-opacity-75 rounded-lg shadow-xl p-4 mb-4 flex justify-between items-center"
-            >
-              <div>
-                <p>
-                  User:{" "}
-                  <span className="text-yellow-400">{game.user.email}</span>
-                </p>
-                <p>
-                  Score: <span className="text-green-400">{game.score}</span>
-                </p>
-                <p>
-                  Level: <span className="text-blue-400">{game.level}</span>
-                </p>
-                <p>
-                  Lines Cleared:{" "}
-                  <span className="text-red-400">{game.linesCleared}</span>
+          {topBestGames.length > 0 ? (
+            topBestGames.map((game, index) => (
+              <div
+                key={index}
+                className="bg-gray-800 bg-opacity-75 rounded-lg shadow-xl p-4 mb-4 flex justify-between items-center"
+              >
+                <div>
+                  <p>
+                    User:{" "}
+                    <span className="text-yellow-400">{game.user.email}</span>
+                  </p>
+                  <p>
+                    Score: <span className="text-green-400">{game.score}</span>
+                  </p>
+                  <p>
+                    Level: <span className="text-blue-400">{game.level}</span>
+                  </p>
+                  <p>
+                    Lines Cleared:{" "}
+                    <span className="text-red-400">{game.linesCleared}</span>
+                  </p>
+                </div>
+                <p className="text-sm text-gray-400">
+                  {new Date(game.createdAt).toLocaleString()}
                 </p>
               </div>
-              <p className="text-sm text-gray-400">
-                {new Date(game.createdAt).toLocaleString()}
-              </p>
-            </div>
-          ))}
+            ))
+          ) : (
+            <p className="text-2xl">No games available</p>
+          )}
         </section>
         <section>
           <h2 className="text-3xl mb-4 text-blue-500">Recent Games</h2>
-          {recentGames.map((game, index) => (
-            <div
-              key={index}
-              className="bg-gray-800 bg-opacity-75 rounded-lg shadow-xl p-4 mb-4 flex justify-between items-center"
-            >
-              <div>
-                <p>
-                  User:{" "}
-                  <span className="text-yellow-400">{game.user.email}</span>
-                </p>
-                <p>
-                  Score: <span className="text-green-400">{game.score}</span>
-                </p>
-                <p>
-                  Level: <span className="text-blue-400">{game.level}</span>
-                </p>
-                <p>
-                  Lines Cleared:{" "}
-                  <span className="text-red-400">{game.linesCleared}</span>
+          {recentGames.length > 0 ? (
+            recentGames.map((game, index) => (
+              <div
+                key={index}
+                className="bg-gray-800 bg-opacity-75 rounded-lg shadow-xl p-4 mb-4 flex justify-between items-center"
+              >
+                <div>
+                  <p>
+                    User:{" "}
+                    <span className="text-yellow-400">{game.user.email}</span>
+                  </p>
+                  <p>
+                    Score: <span className="text-green-400">{game.score}</span>
+                  </p>
+                  <p>
+                    Level: <span className="text-blue-400">{game.level}</span>
+                  </p>
+                  <p>
+                    Lines Cleared:{" "}
+                    <span className="text-red-400">{game.linesCleared}</span>
+                  </p>
+                </div>
+                <p className="text-sm text-gray-400">
+                  {new Date(game.createdAt).toLocaleString()}
                 </p>
               </div>
-              <p className="text-sm text-gray-400">
-                {new Date(game.createdAt).toLocaleString()}
-              </p>
-            </div>
-          ))}
+            ))
+          ) : (
+            <p className="text-2xl">No recent games available</p>
+          )}
         </section>
       </div>
+      <Messages />
     </div>
   );
 }
