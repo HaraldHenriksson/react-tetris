69d71c7982ade91ad0e5fe2b8631e7a601f8385a
Harald Henriksson <harald@espinagar.se>
2024-01-09 14:02:14 +0100

    styling improvments NextTetromino

--- src/components/NextTetromino.tsx
+++ src/components/NextTetromino.tsx
@@ -12,18 +12,24 @@ const NextTetromino: React.FC<NextTetrominoProps> = ({ nextTetrominoType }) => {
   const color = Tetrominos[nextTetrominoType].color;
 
   return (
-    <div className="p-4 bg-gray-800 bg-opacity-75 rounded-lg shadow-xl">
-      {nextTetrominoShape.map((row, rowIndex) => (
-        <div key={rowIndex} className="flex">
-          {row.map((cell, cellIndex) => (
-            <Cell
-              key={`${rowIndex}-${cellIndex}`}
-              filled={!!cell}
-              color={cell ? color : "transparent"}
-            />
-          ))}
-        </div>
-      ))}
+    <div
+      className="p-4 bg-gray-800 bg-opacity-75 rounded-lg shadow-xl flex flex-col items-center justify-center"
+      style={{ width: "160px", height: "200px" }}
+    >
+      <p className="text-white text-lg">Next</p>
+      <div className="flex flex-col justify-center items-center h-full">
+        {nextTetrominoShape.map((row, rowIndex) => (
+          <div key={rowIndex} className="flex justify-center">
+            {row.map((cell, cellIndex) => (
+              <Cell
+                key={`${rowIndex}-${cellIndex}`}
+                filled={!!cell}
+                color={cell ? color : "transparent"}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
