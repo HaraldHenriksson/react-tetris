fcf44ace428382e93cb4c96e0dfa9cad49b31b5d
Harald Henriksson <harald@espinagar.se>
2024-01-10 22:55:32 +0100

    digital font working

--- src/app/(signedin)/game/page.tsx
+++ src/app/(signedin)/game/page.tsx
@@ -228,17 +228,18 @@ export default function Game() {
   }, [isGameOver, score, level, linesCleared]);
 
   return (
-    <div className="bg-customBlue min-h-screen flex justify-center items-center">
-      <div className="text-white font-digital p-4 bg-gray-800 bg-opacity-75 rounded-lg shadow-xl">
+    <div className="bg-customBlue min-h-screen flex justify-center items-center ">
+      <div className="text-white p-4 bg-gray-800 bg-opacity-75 rounded-lg shadow-xl">
         <PausePlayIcon isPaused={isPaused} onClick={togglePause} />
         <p className="text-2xl mb-2">
-          Score: <span className="text-green-400">{score}</span>
+          Score: <span className="text-green-400 font-digital">{score}</span>
         </p>
-        <p className="text-2xl mb-2">
-          Level: <span className="text-blue-400">{level}</span>
+        <p className="text-2xl mb-2 ">
+          Level: <span className="text-blue-400 font-digital">{level}</span>
         </p>
         <p className="text-2xl">
-          Lines: <span className="text-red-400">{linesCleared}</span>
+          Lines:{" "}
+          <span className="text-red-500 font-digital">{linesCleared}</span>
         </p>
       </div>
       <div className="relative w-auto">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       fontFamily: {
-        digital: ["Press Start 2P", "system-ui"],
+        digital: ['"Digital-7"', "system-ui"],
       },
     },
   },
