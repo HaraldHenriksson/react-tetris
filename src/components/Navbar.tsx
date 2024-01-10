837e76262b907b18608f8c575517c8e88ff3321b
Harald Henriksson <harald@espinagar.se>
2024-01-10 23:33:54 +0100

    nav styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -33,7 +33,7 @@ export default async function Navbar() {
         <ul className="flex flex-col md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent">
           <NavItem
             href="/home"
-            className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+            className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
           >
             Home
           </NavItem>
