8503c31bae07abc0422722120d180442ddd67647
Harald Henriksson <harald@espinagar.se>
2024-01-10 16:51:30 +0100

    musicControl component working

--- src/app/(signedin)/layout.tsx
+++ src/app/(signedin)/layout.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "@/components/Navbar";
+import MusicControl from "@/components/MusicControl";
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navbar />
+      <MusicControl />
       <main>{children}</main>
     </>
   );
---
+++ src/components/MusicControl.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+
+const MusicControl = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
+
+  useEffect(() => {
+    // init audio
+    const newAudio = new Audio(
+      "https://ia801407.us.archive.org/1/items/tetris-theme-song/tetris%20theme%20song.mp3"
+    );
+    newAudio.loop = true;
+    setAudio(newAudio);
+  }, []);
+
+  const togglePlay = () => {
+    if (audio) {
+      if (isPlaying) {
+        audio.pause();
+      } else {
+        audio
+          .play()
+          .catch((error) => console.error("Audio playback failed:", error));
+      }
+      setIsPlaying(!isPlaying);
+    }
+  };
+
+  return (
+    <button onClick={togglePlay} className="music-control">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke={isPlaying ? "currentColor" : "gray"}
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="m9 9 10.5-3m0 6.553v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 1 1-.99-3.467l2.31-.66a2.25 2.25 0 0 0 1.632-2.163Zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 1 1-.99-3.467l2.31-.66A2.25 2.25 0 0 0 9 15.553Z"
+        />
+      </svg>
+    </button>
+  );
+};
+
+export default MusicControl;
