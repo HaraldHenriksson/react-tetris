2e9a5b82fa9b9586ccf7b80a50739f2bf9c6ecf8
Harald Henriksson <harald@espinagar.se>
2024-01-10 20:36:31 +0100

    protected routes and redirect if !auth user

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,10 @@ import { NextResponse } from "next/server";
 
 import type { NextRequest } from "next/server";
 
+const protectedRoutes = ["/game", "/home", "/leaderboard", "/profile"];
+
 export async function middleware(req: NextRequest) {
+  const requestUrl = new URL(req.url);
   const res = NextResponse.next();
 
   // Create a Supabase client configured to use cookies
@@ -11,7 +14,13 @@ export async function middleware(req: NextRequest) {
 
   // Refresh session if expired - required for Server Components
   // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-session-with-middleware
-  await supabase.auth.getSession();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (protectedRoutes.includes(req.nextUrl.pathname) && !session) {
+    return NextResponse.redirect(`${requestUrl.origin}/sign-in`, 307);
+  }
 
   return res;
 }
