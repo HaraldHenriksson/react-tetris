e36cf48d1a0264870fbc8bab097ff62a71d58ab6
Harald Henriksson <harald@espinagar.se>
2024-01-11 13:10:16 +0100

    home page styling

--- src/app/(signedin)/home/page.tsx
+++ src/app/(signedin)/home/page.tsx
@@ -5,21 +5,21 @@ import Link from "next/link";
 
 export default async function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen bg-customBlue p-6">
+    <main className="box-content flex flex-col items-center h-screen bg-customBlue p-6">
       <header className="text-center">
-        <h1 className="text-7xl mt-6 mb-6 font-blackOpsOne text-blue-500">
+        <h1 className="text-7xl mt-32 mb-6 font-blackOpsOne text-blue-500">
           React Tetris
         </h1>
-        <p>A modern take on the classic game</p>
+        <p className="mb-10">A modern take on the classic game</p>
       </header>
 
       <Link legacyBehavior href="/game">
-        <a className="inline-block mt-4 px-6 py-2 text-white bg-blue-500 rounded hover:bg-blue-600">
+        <a className="inline-block mt-40 px-6 py-2 text-white bg-blue-500 rounded hover:bg-blue-600">
           Start Game
         </a>
       </Link>
 
-      <footer className="text-gray-600">
+      <footer className="mt-auto text-gray-600">
         <p>React Tetris © 2023</p>
       </footer>
     </main>
