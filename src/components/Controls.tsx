4c1d8dc44cf1d88a1e3b30e69cc8b85c72c78258
Harald Henriksson <harald@espinagar.se>
2024-01-12 13:03:35 +0100

    move icons

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { CloseIcon, InfoIcon } from "@/icons/iconst";
 import { useState } from "react";
 
 export default function ControlsModal() {
@@ -8,20 +9,7 @@ export default function ControlsModal() {
   if (!showModal) {
     return (
       <button onClick={() => setShowModal(true)}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
-          />
-        </svg>
+        <InfoIcon />
       </button>
     );
   }
@@ -33,20 +21,7 @@ export default function ControlsModal() {
           className="absolute top-2 right-2"
           onClick={() => setShowModal(false)}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M6 18 18 6M6 6l12 12"
-            />
-          </svg>
+          <CloseIcon />
         </button>
 
         <h2 className="font-bold mb-2">Single Player Game:</h2>
