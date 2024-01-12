1d994049186bcb35f8d78fb2b49c1344f01b0f21
Harald Henriksson <harald@espinagar.se>
2024-01-12 12:57:49 +0100

    icons and modal styling

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -6,12 +6,49 @@ export default function ControlsModal() {
   const [showModal, setShowModal] = useState(false);
 
   if (!showModal) {
-    return <button onClick={() => setShowModal(true)}>Controls</button>;
+    return (
+      <button onClick={() => setShowModal(true)}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
+          />
+        </svg>
+      </button>
+    );
   }
 
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
-      <div className="backdrop-filter backdrop-blur-lg bg-white bg-opacity-30 p-8 rounded shadow-lg">
+      <div className="backdrop-filter backdrop-blur-lg bg-white bg-opacity-30 p-8 rounded shadow-lg w-1/4">
+        <button
+          className="absolute top-2 right-2"
+          onClick={() => setShowModal(false)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6 18 18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+
         <h2 className="font-bold mb-2">Single Player Game:</h2>
         <ul>
           <li>Move Left: ArrowLeft</li>
@@ -40,8 +77,6 @@ export default function ControlsModal() {
             <li>Drop: Space</li>
           </ul>
         </ul>
-
-        <button onClick={() => setShowModal(false)}>Close</button>
       </div>
     </div>
   );
