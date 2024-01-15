d3c0be11d0405c3c57bb89ed9aabaa8b96223c4a
Harald Henriksson <harald@espinagar.se>
2024-01-15 15:50:16 +0100

    text-white

--- src/components/UserDropdown.tsx
+++ src/components/UserDropdown.tsx
@@ -33,7 +33,7 @@ const UserDropdown = ({ user }: UserDropdownProps) => {
     <div className="relative">
       <button
         onClick={toggleDropdown}
-        className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+        className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
       >
         {user.email}
       </button>
